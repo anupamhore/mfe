bb2ee05efbb59cd62d08653a037139154de400e5
Anupam Hore <anupam.hore@pos.com.my>
2025-03-22 22:53:52 +0800

    initial commit

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/, //any file which ends with js or mjs will be processed by babel
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['@babel/preset-react','@babel/preset-env'], //convert es17,es18.. to es5
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/, //any file which ends with js or mjs will be processed by babel
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['@babel/preset-react','@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            }
+        ]
+    }
+}
